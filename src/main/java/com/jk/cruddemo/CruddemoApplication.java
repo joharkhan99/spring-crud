26f59775f0a25947b4f86ea516ce268bfcfb5ff4
single-byte <70197335+single-byte@users.noreply.github.com>
2023-08-21 10:42:27 +0500

    added auto create and update using JPA

--- src/main/java/com/jk/cruddemo/CruddemoApplication.java
+++ src/main/java/com/jk/cruddemo/CruddemoApplication.java
@@ -21,8 +21,8 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
 //			createStudent(studentDAO);
-//			createMultipleStudents(studentDAO);
-			
+			createMultipleStudents(studentDAO);
+
 //			readStudent(studentDAO);
 //			queryForStudents(studentDAO);
 //			getStudentsByLastName(studentDAO);
